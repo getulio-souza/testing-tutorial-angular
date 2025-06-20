3325478cb99df80c85fe02773068907c0308c181
Getulio Souza <getuliosouza8@gmail.com>
2025-06-20 11:46:40 -0300

    feature: pipe

---
+++ src/app/pipes/strength/stength.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { StengthPipe } from './stength.pipe';
+
+describe('StengthPipe', () => {
+  it('create an instance', () => {
+    const pipe = new StengthPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
