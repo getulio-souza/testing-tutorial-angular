25e5b7fd838c3eea37fdcd39d725b9b5e922e1ea
Getulio Souza <getuliosouza8@gmail.com>
2025-07-16 16:19:39 -0300

    16-07-25

--- src/app/posts/post.component.spec.ts
+++ src/app/posts/post.component.spec.ts
@@ -2,14 +2,24 @@ import { of } from "rxjs";
 import { Post } from "../models/post"
 import { PostsComponent } from "./posts.component";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { PostComponent } from "../components/post/post.component";
 import { PostService } from "../Services/post.service";
+import { Component, Input } from "@angular/core";
+import { By } from "@angular/platform-browser";
+import { PostComponent } from "../components/post/post.component";
 
 describe('post component', () => {
   let POSTS: Post[];
   let component: PostsComponent;
   let mockPostService: any;
   let fixture: ComponentFixture<PostsComponent>
+
+  // @Component({
+  //   selector: 'app-post',
+  //   template: '<div></div>'
+  // })
+  // class FakePostComponent {
+  //   @Input() post!: Post;
+  // }
 
   //before each is used to initilize the component
   beforeEach(() => {
@@ -34,7 +44,7 @@ describe('post component', () => {
     mockPostService = jasmine.createSpyObj('PostService',['getPosts', 'deletePost']);
 
     TestBed.configureTestingModule({
-      declarations: [PostsComponent],
+      declarations: [PostsComponent, PostComponent],
       providers: [
         PostsComponent,
         {
@@ -49,6 +59,21 @@ describe('post component', () => {
   });
 
   describe('delete method', () => {
+
+    it('should create exact same number of post component with Posts', ()=> {
+      mockPostService.getPosts.and.returnValue(of(POSTS));
+      fixture.detectChanges()
+      const postComponents = fixture.debugElement.queryAll(By.directive(PostComponent));
+      expect(postComponents.length).toEqual(POSTS.length)
+    })
+
+    // it('should create one post element for each post', ()=> {
+    //   mockPostService.getPosts.and.returnValue(of(POSTS));
+    //   fixture.detectChanges()
+    //   const debugElement = fixture.debugElement;
+    //   const postsElement = debugElement.queryAll(By.css('.posts'));
+    //   expect(postsElement.length).toBe(POSTS.length)
+    // })
 
     it('should delete the selected post from the posts', () => {
       component.posts = [...POSTS]
