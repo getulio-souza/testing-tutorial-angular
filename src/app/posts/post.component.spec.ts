43cf1d72c545b31eb2286787e2988edc01c34dc2
Getulio Souza <getuliosouza8@gmail.com>
2025-07-17 11:33:27 -0300

    feature: testing-title-from-post

--- src/app/posts/post.component.spec.ts
+++ src/app/posts/post.component.spec.ts
@@ -1,11 +1,12 @@
 import { of } from "rxjs";
 import { Post } from "../models/post"
 import { PostsComponent } from "./posts.component";
-import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ComponentFixture, fakeAsync, TestBed, tick } from "@angular/core/testing";
 import { PostService } from "../Services/post.service";
 import { Component, Input } from "@angular/core";
 import { By } from "@angular/platform-browser";
 import { PostComponent } from "../components/post/post.component";
+import { RouterTestingModule } from "@angular/router/testing";
 
 describe('post component', () => {
   let POSTS: Post[];
@@ -45,6 +46,7 @@ describe('post component', () => {
 
     TestBed.configureTestingModule({
       declarations: [PostsComponent, PostComponent],
+      imports: [RouterTestingModule],
       providers: [
         PostsComponent,
         {
@@ -60,11 +62,25 @@ describe('post component', () => {
 
   describe('delete method', () => {
 
-    it('should create exact same number of post component with Posts', ()=> {
+    it('should create exact same number of Post component with posts',()=> {
       mockPostService.getPosts.and.returnValue(of(POSTS));
-      fixture.detectChanges()
-      const postComponents = fixture.debugElement.queryAll(By.directive(PostComponent));
-      expect(postComponents.length).toEqual(POSTS.length)
+      fixture.detectChanges() //like calling ngOnInit from the component 
+      const postComponent = fixture.debugElement.queryAll(By.directive(PostComponent))
+      expect(postComponent.length).toEqual(POSTS.length)
+    })
+
+    //testing a single post 
+
+    it('should test if each post title matches each post title from the POSTS', ()=> {
+      mockPostService.getPosts.and.returnValue(of(POSTS));
+      fixture.detectChanges();
+      const postComponentDebugElement = fixture.debugElement.queryAll(By.directive(PostComponent));
+
+      //iterates the POSTS array to check each title individuality
+      for(let i= 0; i< postComponentDebugElement.length; i++){
+        let postComponentInstance = postComponentDebugElement[i].componentInstance as PostComponent
+        expect(postComponentInstance.post.title).toBe(POSTS[i].title)
+      }
     })
 
     // it('should create one post element for each post', ()=> {
